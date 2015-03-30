6cd7de9b8f153497ece65b90ce0e8a7ec065a19e
unknown <gracevente11@OLINLAB204-02.augustana.edu>
2015-03-30 14:46:16 -0500

    Initial commit

---
+++ app/src/androidTest/java/SoccerBall.java
@@ -0,0 +1,13 @@
+/**
+ * Created by gracevente11 on 3/27/2015.
+ */
+public class SoccerBall {
+        private int positionX;
+        private int positionY;
+        private int velocityX;
+        private int velocityY;
+
+    //constructor
+
+
+}
